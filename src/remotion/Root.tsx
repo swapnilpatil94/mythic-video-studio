1ef9ccc7d9c5954c0c7d9fad107a105b583913b5
Swapnil Patil <47409544+swapnilpatil94@users.noreply.github.com>
2026-09-05 23:08:44 +0530

    feat: make Remotion duration manifest-driven

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {MythicShort} from './MythicShort';
+import {runtimeManifest} from './runtime-manifest';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="MythicShort"
       component={MythicShort}
-      durationInFrames={75 * 30}
+      durationInFrames={Math.round(runtimeManifest.duration_seconds * 30)}
       fps={30}
       width={1080}
       height={1920}
-      defaultProps={{manifestPath: 'examples/karna-short.json'}}
+      defaultProps={{manifest: runtimeManifest}}
     />
   );
 };
