c43d01cc37d2e8b8e2bca794b52ff03ee86ff7fc
Swapnil Patil <47409544+swapnilpatil94@users.noreply.github.com>
2026-09-08 10:00:53 +0530

    feat: register drawing-stage test composition

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {MythicShort} from './MythicShort';
+import {DrawingStageTest} from './DrawingStageTest';
 import {runtimeManifest} from './runtime-manifest';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="MythicShort"
-      component={MythicShort}
-      durationInFrames={Math.round(runtimeManifest.duration_seconds * 30)}
-      fps={30}
-      width={1080}
-      height={1920}
-      defaultProps={{manifest: runtimeManifest}}
-    />
+    <>
+      <Composition
+        id="MythicShort"
+        component={MythicShort}
+        durationInFrames={Math.round(runtimeManifest.duration_seconds * 30)}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{manifest: runtimeManifest}}
+      />
+      <Composition
+        id="DrawingStageTest"
+        component={DrawingStageTest}
+        durationInFrames={15 * 30}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+    </>
   );
 };
