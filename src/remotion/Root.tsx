d834c50ec3ee78ec654e8abae90dc163556f0b9e
Swapnil Patil <47409544+swapnilpatil94@users.noreply.github.com>
2026-09-08 12:51:12 +0530

    feat: register production drawing test

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Composition} from 'remotion';
 import {MythicShort} from './MythicShort';
 import {DrawingStageTest} from './DrawingStageTest';
+import {ProductionDrawingTest} from './ProductionDrawingTest';
 import {runtimeManifest} from './runtime-manifest';
 
 export const RemotionRoot: React.FC = () => {
@@ -19,6 +20,14 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="DrawingStageTest"
         component={DrawingStageTest}
+        durationInFrames={15 * 30}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+      <Composition
+        id="ProductionDrawingTest"
+        component={ProductionDrawingTest}
         durationInFrames={15 * 30}
         fps={30}
         width={1080}
